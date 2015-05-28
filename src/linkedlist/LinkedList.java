1ccdfa2612d85fbf59f2e6d2539ec990eabaf09d
Kristian Sooklal <kristiansooklal@gmail.com>
2015-05-28 17:11:05 -0400

    LinkedList project
    LinkedList Java implementation

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,505 @@
+package linkedlist;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedList<E> implements List<E>, Deque<E>, Cloneable, java.io.Serializable {
+	private static final long serialVersionUID = 1234234234L;
+	private Node<E> head = null;
+	private int size = 0;
+	
+	private static class Node<T> {
+		private T data;
+		private Node<T> next;
+
+		private Node(T data){
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public LinkedList(){
+		this.size = 0;
+		this.head = null;
+	}
+
+	@Override
+	public void addFirst(E value) {
+		if (this.head == null){
+			this.head = new Node<E>(value);
+		} else {
+			Node<E> node = new Node<E>(value);
+			node.next = this.head;
+			this.head = node;
+		}
+		size++;
+	}
+
+	@Override
+	public void addLast(E value) {
+		if (this.head == null){
+			this.head = new Node<E>(value);
+		} else {
+			Node<E> pointer = this.head;
+			while (pointer.next != null){
+				pointer = pointer.next;
+			}
+			pointer.next = new Node<E>(value);
+		}
+		size++;
+	}
+
+	@Override
+	public boolean offerFirst(E value) {
+		if (this.head == null){
+			this.head = new Node<E>(value);
+		} else {
+			Node<E> node = new Node<E>(value);
+			node.next = this.head;
+			this.head = node;
+		}
+		size++;
+		return true;
+	}
+
+	@Override
+	public boolean offerLast(E value) {
+		if (this.head == null){
+			this.head = new Node<E>(value);
+		} else {
+			Node<E> pointer = this.head;
+			while (pointer.next != null){
+				pointer = pointer.next;
+			}
+			pointer.next = new Node<E>(value);
+		}
+		size++;
+		return true;
+	}
+
+	@Override
+	public E removeFirst() {
+		if (this.head == null){
+			throw new UnsupportedOperationException("The list is empty, no first element found.");
+		}
+		E data = this.head.data;
+		this.head = this.head.next;
+		size--;
+		return data;
+	}
+
+	@Override
+	public E removeLast() {
+		if (this.head == null){
+			return null;
+		}
+		E data = this.head.data;
+		this.head = this.head.next;
+		size--;
+		return data;
+	}
+
+	@Override
+	public E pollFirst() {
+		return (this.head == null || this.size == 0) ? null : this.removeFirst();
+	}
+
+	@Override
+	public E pollLast() {
+		return (this.head == null || this.size == 0) ? null : this.removeLast();
+	}
+
+	@Override
+	public E getFirst() {
+		if (this.head == null){
+			throw new UnsupportedOperationException("The list is empty, no first element found.");
+		} else {
+			return this.head.data;
+		}
+	}
+
+	@Override
+	public E getLast() {
+		if (this.head == null){
+			throw new UnsupportedOperationException("The list is empty, no first element found.");
+		} else {
+			Node<E> pointer = this.head;
+			while (pointer.next == null){
+				pointer = pointer.next;
+			}
+			return pointer.data;
+		}
+	}
+
+	@Override
+	public E peekFirst() {
+		return this.head == null ? null: this.head.data;
+	}
+
+	@Override
+	public E peekLast() {
+		if (this.head == null){
+			return null;
+		}
+		Node<E> pointer = this.head;
+		while (pointer.next == null){
+			pointer = pointer.next;
+		}
+		return pointer.data;
+	}
+
+	@Override
+	public boolean removeFirstOccurrence(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeLastOccurrence(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offer(E value) {
+		return this.offerLast(value);
+	}
+
+	@Override
+	public E remove() {
+		return removeFirst();
+	}
+
+	@Override
+	public E poll() {
+		return pollFirst();
+	}
+
+	@Override
+	public E element() {
+		if (this.head == null){
+			throw new UnsupportedOperationException("The list is empty, no first element found.");
+		}
+		return this.head.data;
+	}
+
+	@Override
+	public E peek() {
+		return (this.head == null) ? null : this.head.data;
+	}
+
+	@Override
+	public void push(E value) {
+		this.addFirst(value);
+	}
+
+	@Override
+	public E pop() {
+		return this.removeFirst();
+	}
+
+	@Override
+	public Iterator<E> descendingIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.head == null;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if (this.head == null){
+			return false;
+		}
+		Node<E> pointer = this.head;
+		while (pointer.next != null){
+			if (o == null){
+				if (pointer.data == null){
+					return true;
+				}
+			} else if (o.equals(pointer.data)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		if (this.size == 0 || this.head == null){
+			return new Object[0];
+		}
+		Object [] array = new Object[size];
+		Node<E> pointer = this.head;
+		int arrayIndex = 0;
+		while (pointer.next != null){
+			array[arrayIndex++] = pointer.data;
+			pointer = pointer.next;
+		}
+		return array;
+	}
+
+	@SuppressWarnings({ "unchecked" })
+	@Override
+	public <T> T[] toArray(T[] a) {
+		if (this.size == 0 || this.head == null){
+			return Arrays.copyOf(a, 0);
+		}
+		T[] array = Arrays.copyOf(a, this.size);
+		Object[] objectsArray = this.toArray();
+		int arrayIndex = 0;
+		for (Object object: objectsArray){
+			array[arrayIndex++] = (T) object;
+		}
+		return array;
+	}
+
+	@Override
+	public boolean add(E value) {
+		if (this.head == null){
+			this.head = new Node<E>(value);
+			size++;
+			return true;
+		}
+		Node<E> pointer = this.head;
+		while (pointer.next != null){
+			pointer = pointer.next;
+		}
+		pointer.next = new Node<E>(value);
+		size++;
+		return true;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (this.head == null){
+			return false;
+		}
+		if (this.head.next == null){
+			if (o == null){
+				if (this.head.data == null){
+					this.head = null;
+					size--;
+					return true;
+				}
+			} else if (o.equals(this.head.data)) {
+				this.head = null;
+				size--;
+				return true;
+			}
+			return false;
+		}
+		
+		Node<E> prev = this.head;
+		Node<E> curr = this.head.next;
+		
+		while (curr != null){
+			if (o == null){
+				if (curr.data == null){
+					prev.next = curr.next;
+					size--;
+					return true;
+				}
+			} else if (o.equals(this.head.data)) {
+				prev.next = curr.next;
+				size--;
+				return true;
+			}
+			prev = curr;
+			curr = curr.next;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		if (c == null){
+			throw new NullPointerException ("Null collection provided.");
+		}
+		if (c.isEmpty()){
+			return false;
+		}
+		Iterator<?> iterator = c.iterator();
+		while (iterator.hasNext()){
+			if (!this.contains(iterator.next())){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		if (c == null){
+			throw new NullPointerException ("Null collection provided.");
+		}
+		if (c.isEmpty()){
+			return false;
+		}
+		int originalSize = this.size;
+		Iterator<? extends E> iterator = c.iterator();
+		while (iterator.hasNext()){
+			this.add(iterator.next());
+		}
+		return originalSize == this.size;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		if (index < 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Index (" + index + ") is out of range for list of size: " + size);
+		}
+		if (c == null){
+			throw new NullPointerException ("Null collection provided.");
+		}
+		if (c.isEmpty()){
+			return false;
+		}
+		int originalSize = this.size;
+		Iterator<? extends E> iterator = c.iterator();
+		while (iterator.hasNext()){
+			int previousSize = this.size;
+			this.add(index, iterator.next());
+			if (this.size > previousSize){
+				index++;
+			}
+		}
+		return originalSize == this.size;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		this.head = null;
+		size = 0;
+	}
+
+	@Override
+	public E get(int index) {
+		if (index < 0 || index >= size){
+			throw new IndexOutOfBoundsException("Index (" + index + ") is out of range for list of size: " + size);
+		}
+		if (index == 0){
+			return this.head.data;
+		}
+		int elementIndex = 0;
+		Node<E> pointer = this.head;
+		while (pointer != null){
+			if (elementIndex == index){
+				return pointer.data;
+			}
+			pointer = pointer.next;
+			elementIndex++;
+		}
+		return null;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		if (index < 0 || index >= size){
+			throw new IndexOutOfBoundsException("Index (" + index + ") is out of range for list of size: " + size);
+		}
+		if (index == 0){
+			E oldData = this.head.data;
+			this.head.data = element;
+			return oldData;
+		}
+		int elementIndex = 0;
+		Node<E> pointer = this.head;
+		while (pointer != null){
+			if (elementIndex == index){
+				E oldData = pointer.data;
+				pointer.data = element;
+				return oldData;
+			}
+			pointer = pointer.next;
+			elementIndex++;
+		}
+		return null;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String toString(){
+		if (this.head == null){
+			return "{}";
+		}
+		StringBuffer start = new StringBuffer("{" + this.head.data);
+		Node<E> pointer = this.head.next;
+		while (pointer != null){
+			start.append(", " + pointer.data);
+			pointer = pointer.next;
+		}
+		return start.append("}").toString();
+	}
+}
---
+++ test/linkedlist/LinkedListUnitTest.java
@@ -0,0 +1,267 @@
+package linkedlist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkedListUnitTest {
+
+	@SuppressWarnings(value = "rawtypes")
+	@Test
+	public void testConstructor(){
+		LinkedList<String> list = new LinkedList<String>();
+		assertNotEquals(null, list);
+		assertEquals(0, list.size());
+		
+		LinkedList<Double> newList = new LinkedList<Double>();
+		assertNotEquals(null, newList);
+		
+		LinkedList untypedList = new LinkedList();
+		assertEquals(0, untypedList.size());
+	}
+
+	@Test
+	public void testAddFirst() {
+		LinkedList<int []> listOfArrays = new LinkedList<int []>();
+		assertEquals(0, listOfArrays.size());
+		
+		listOfArrays.addFirst(new int [] {2, 234});
+		assertEquals(1, listOfArrays.size());
+		int [] storedArray = listOfArrays.get(0);
+		assertEquals(2, storedArray[0]);
+		assertEquals(234, storedArray[1]);
+		
+		listOfArrays.addFirst(new int [] {9129, 123901, 12309});
+		assertEquals(2, listOfArrays.size());
+		storedArray = listOfArrays.get(0);
+		assertEquals(9129, storedArray[0]);
+		assertEquals(123901, storedArray[1]);
+		assertEquals(12309, storedArray[2]);
+		
+		int previousStoredArray [] = listOfArrays.get(1);
+		assertEquals(2, previousStoredArray[0]);
+		assertEquals(234, previousStoredArray[1]);
+		
+	}
+
+	@Test
+	public void testAddLast() {
+		LinkedList<int []> listOfArrays = new LinkedList<int []>();
+		assertEquals(0, listOfArrays.size());
+		
+		listOfArrays.addLast(new int [] {2, 234});
+		assertEquals(1, listOfArrays.size());
+		int [] storedArray = listOfArrays.get(0);
+		assertEquals(2, storedArray[0]);
+		assertEquals(234, storedArray[1]);
+		
+		listOfArrays.addLast(new int [] {9129, 123901, 12309});
+		assertEquals(2, listOfArrays.size());
+		
+		int previousStoredArray [] = listOfArrays.get(0);
+		assertEquals(2, previousStoredArray[0]);
+		assertEquals(234, previousStoredArray[1]);
+		
+		int [] newStoredArray = listOfArrays.get(1);
+		assertEquals(9129, newStoredArray[0]);
+		assertEquals(123901, newStoredArray[1]);
+		assertEquals(12309, newStoredArray[2]);
+
+	}
+
+	@Test
+	public void testOfferFirst() {
+	}
+
+	@Test
+	public void testOfferLast(){
+	}
+
+	@Test
+	public void testRemoveFirst(){
+	}
+
+	@Test
+	public void testRemoveLast() {
+	}
+
+	@Test
+	public void testPollFirst() {
+	}
+
+	@Test
+	public void testPollLast() {
+	}
+
+	@Test
+	public void testGetFirst() {
+	}
+
+	@Test
+	public void testGetLast() {
+	}
+
+	@Test
+	public void testPeekFirst() {
+	}
+
+	@Test
+	public void testPeekLast() {
+	}
+
+	@Test
+	public void testRemoveFirstOccurrence() {
+	}
+
+	@Test
+	public void testRemoveLastOccurrence() {
+	}
+
+	@Test
+	public void testOffer() {
+	}
+
+	@Test
+	public void testRemove() {
+	}
+
+	@Test
+	public void testPoll() {
+	}
+
+	@Test
+	public void testElement() {
+	}
+
+	@Test
+	public void testPeek() {
+	}
+
+	@Test
+	public void testPush() {
+	}
+
+	@Test
+	public void testPop() {
+	}
+
+	@Test
+	public void testDescendingIterator() {
+	}
+
+	@Test
+	public void testSize() {
+	}
+
+	@Test
+	public void testIsEmpty() {
+		
+	}
+
+	@Test
+	public void testContains() {
+	
+	}
+
+	@Test
+	public void testIterator() {
+		
+	}
+
+	@Test
+	public void testToArray() {
+	
+	}
+
+	@Test
+	public void testToArray2() {
+	}
+
+	@Test
+	public void testAdd2() {
+	}
+
+	@Test
+	public void testRemove2() {
+		
+	}
+
+	@Test
+	public void testContainsAll() {
+		
+	}
+
+	@Test
+	public void testAddAll() {
+	}
+
+	@Test
+	public void testAddAll2() {
+		
+	}
+
+	@Test
+	public void testRemoveAll() {
+		
+	}
+
+	@Test
+	public void testRetainAll() {
+		
+	}
+
+	@Test
+	public void testClear() {
+		
+	}
+
+	@Test
+	public void testGet(){
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("First");
+		list.add("Second");
+		list.add("Third");
+		list.add("Fourth");
+		assertEquals("Third", list.get(2));
+	}
+
+	@Test
+	public void testSet() {
+		
+	}
+
+	@Test
+	public void testAdd() {
+		
+	}
+
+	@Test
+	public void testRemove3(){
+		
+	}
+
+	@Test
+	public void testIndexOf(){
+		
+	}
+
+	@Test
+	public void testlastIndexOf(){
+	}
+
+	@Test
+	public void testListIterator() {
+		
+	}
+
+	@Test
+	public void testListIterator2() {
+		
+	}
+
+	@Test
+	public void testSubList() {
+		
+	}
+
+}
